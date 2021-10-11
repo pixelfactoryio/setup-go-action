df00e8541ceafc38206c1b8f0487888eb69ffcce
Amine Benseddik <amine@qonto.com>
2021-10-11 22:01:18 +0200

    support mutli arch

--- run.sh
+++ run.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-set -xe
-
-echo ${INPUT_GO_VERSION}
 
 DEBIAN_FRONTEND=noninteractive
 GO_VERSION="${INPUT_GO_VERSION:-1.17.2}"
-WORKDIR=$(mktemp -d)
+ARCH="${INPUT_ARCH:-amd64}"
 
-# https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
-# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
-# export PATH=$PATH:/usr/local/go/bin
+WORKDIR=$(mktemp -d)
 
 apt-get update -yqq
 apt-get install -yqq curl
 
 # Install Go
-curl -sSL -o ${WORKDIR}/go${GO_VERSION}.linux-amd64.tar.gz https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+curl -sSL -o ${WORKDIR}/go${GO_VERSION}.linux-${ARCH}.tar.gz https://dl.google.com/go/go${GO_VERSION}.linux-${ARCH}.tar.gz
 rm -rf /usr/local/go 
-tar -C /usr/local -xzf ${WORKDIR}/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf ${WORKDIR}/go${GO_VERSION}.linux-${ARCH}.tar.gz
 
 # Update $PATH
 echo "PATH=$PATH:/usr/local/go/bin" >> /opt/stepssharedfolder/sharedEnv 
